5cac207b38318e852d86d5350733320e47904be2
Dimitar Banev <banev_dimitar@mail.com>
2025-09-19 03:29:39 +0300

    Add tmux-navigator

---
+++ .config/nvim/lua/devastion/plugins/tmux-navigator.lua
@@ -0,0 +1,7 @@
+vim.pack.add({ "https://github.com/christoomey/vim-tmux-navigator" }, { confirm = false })
+
+vim.keymap.set("n", "<C-h>", "<cmd><C-U>TmuxNavigateLeft<cr>")
+vim.keymap.set("n", "<C-j>", "<cmd><C-U>TmuxNavigateDown<cr>")
+vim.keymap.set("n", "<C-k>", "<cmd><C-U>TmuxNavigateUp<cr>")
+vim.keymap.set("n", "<C-l>", "<cmd><C-U>TmuxNavigateRight<cr>")
+vim.keymap.set("n", "<C-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>")
