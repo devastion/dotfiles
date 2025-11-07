af549f228a72095da937a7ce63ab107036a4522c
Dimitar Banev <banev_dimitar@mail.com>
2025-11-07 09:43:34 +0200

    Add eza and bat to ansible

--- .config/nvim/lua/devastion/core/filetypes.lua
+++ .config/nvim/lua/devastion/core/filetypes.lua
@@ -4,10 +4,24 @@ vim.filetype.add({
   },
   pattern = {
     ["%.env%.[%w_.-]+"] = "dotenv",
+
+    -- github actions
     [".*/%.github/workflows/.*%.ya?ml"] = "yaml.ghaction",
+
+    -- docker
     [".*/docker%-compose%.ya?ml"] = "yaml.docker-compose",
     [".*/compose%.ya?ml"] = "yaml.docker-compose",
-    [".*/tasks/.*.ya?ml"] = "yaml.ansible",
-    [".*/playbooks/.*.ya?ml"] = "yaml.ansible",
+
+    -- ansible
+    [".*/defaults/.*%.ya?ml"] = "yaml.ansible",
+    [".*/host_vars/.*%.ya?ml"] = "yaml.ansible",
+    [".*/group_vars/.*%.ya?ml"] = "yaml.ansible",
+    [".*/group_vars/.*/.*%.ya?ml"] = "yaml.ansible",
+    [".*/playbook.*%.ya?ml"] = "yaml.ansible",
+    [".*/playbooks/.*%.ya?ml"] = "yaml.ansible",
+    [".*/roles/.*/tasks/.*%.ya?ml"] = "yaml.ansible",
+    [".*/roles/.*/handlers/.*%.ya?ml"] = "yaml.ansible",
+    [".*/tasks/.*%.ya?ml"] = "yaml.ansible",
+    [".*/molecule/.*%.ya?ml"] = "yaml.ansible",
   },
 })
