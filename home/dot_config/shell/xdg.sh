24d1ce7ca4b88d11c55ba5debd5c1e2b61fbbef1
Dimitar Banev <banev_dimitar@mail.com>
2026-03-30 02:26:59 +0300

    Move xdg folder creation to separate function

---
+++ home/dot_config/shell/xdg.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-$HOME/.xdg}"
+export XDG_PROJECTS_DIR="${XDG_PROJECTS_DIR:-$HOME/Projects}"
+
+xdg_dirs=(
+  "$XDG_CONFIG_HOME"
+  "$XDG_DATA_HOME"
+  "$XDG_STATE_HOME"
+  "$XDG_CACHE_HOME"
+  "$XDG_RUNTIME_DIR"
+  "$XDG_PROJECTS_DIR"
+  "$HOME/.local/bin"
+)
+
+for dir in "${xdg_dirs[@]}"; do
+  [[ ! -d "$dir" ]] && mkdir -p "$dir"
+done
