2311b82addcc8f13e7119aaabb3fe21deee1610f
Dimitar Banev <banev_dimitar@mail.com>
2026-03-29 21:19:08 +0300

    Tweak install.sh script

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ```bash
 chezmoi init --apply --source ~/.dotfiles git@github.com:devastion/dotfiles.git
 ```
+
+or
+
+```bash
+curl -fsSL https://raw.githubusercontent.com/devastion/dotfiles/refs/heads/main/install.sh | sh
+```
--- install.sh
+++ install.sh
@@ -20,11 +20,4 @@ if ! chezmoi="$(command -v chezmoi)"; then
   unset chezmoi_install_script bin_dir
 fi
 
-# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
-script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-
-set -- init --apply --source="${script_dir}"
-
-echo "Running 'chezmoi $*'" >&2
-# exec: replace current process with chezmoi
-exec "$chezmoi" "$@"
+exec $chezmoi init --apply --source ~/.dotfiles https://github.com/devastion/dotfiles.git
