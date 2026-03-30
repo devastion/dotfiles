70ec10f387cd0ed1b03c91d1cf604640f822f910
Dimitar Banev <banev_dimitar@mail.com>
2026-03-30 21:13:26 +0300

    Keep only bare minimum in ~/.zshenv

--- home/.chezmoiscripts/run_once_after_init-xdg_dirs.sh
+++ home/.chezmoiscripts/run_once_after_init-xdg_dirs.sh
@@ -9,7 +9,8 @@ XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
 XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-$HOME/.xdg}"
-XDG_PROJECTS_DIR="${XDG_PROJECTS_DIR:-$HOME/Projects}"
+PROJECTS_DIR="${PROJECTS_DIR:-$HOME/Projects}"
+LOCAL_BIN="$HOME/.local/bin"
 
 for dir in \
   "$XDG_CONFIG_HOME" \
@@ -17,7 +18,9 @@ for dir in \
   "$XDG_STATE_HOME" \
   "$XDG_CACHE_HOME" \
   "$XDG_RUNTIME_DIR" \
-  "$XDG_PROJECTS_DIR" \
-  "$HOME/.local/bin"; do
+  "$PROJECTS_DIR" \
+  "$LOCAL_BIN"; do
   mkdir -p "$dir"
 done
+
+chmod 0700 "$XDG_RUNTIME_DIR"
